292a0d9b8ca5efb4a762403d2dd300e947831678
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-07-14 14:53:32 +0300

    Backend changes, order notifications, user data fetch, user registrations

---
+++ troyka-shop/config/plugins.ts
@@ -0,0 +1,31 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "threeoyka@gmail.com",
+        defaultReplyTo: "threeoyka@gmail.com",
+      },
+    },
+  },
+  // ...
+});
---
+++ troyka-shop/src/api/order/controllers/order.ts
@@ -0,0 +1,114 @@
+("use strict");
+import { v4 as uuidv4 } from "uuid";
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+/**
+ * order controller
+ */
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, paymentMethod, status, addressInfo, user, totalPrice } =
+      ctx.request.body;
+
+    try {
+      if (paymentMethod === "card") {
+        const orderId = await uuidv4();
+
+        const lineItems = await Promise.all(
+          products.map(async (product) => {
+            const item = await strapi
+              .service("api::product.product")
+              .findOne(product.id);
+
+            console.log("this is item------->", item);
+            console.log("this is product------->", product);
+
+            return {
+              price_data: {
+                currency: "bgn",
+                product_data: {
+                  name: item.name,
+                },
+                unit_amount: Math.round(item.price * 500),
+              },
+              quantity: product.quantity,
+            };
+          })
+        );
+
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ["card"],
+          mode: "payment",
+          success_url: process.env.CLIENT_URL + `/success`,
+          cancel_url: process.env.CLIENT_URL + "/failed",
+          line_items: lineItems,
+        });
+
+        await strapi.service("api::order.order").create({
+          data: {
+            products,
+            stripeId: session.id,
+            paymentMethod,
+            orderId,
+            status,
+            addressInfo,
+            user,
+            totalPrice,
+          },
+        });
+
+        return { stripeSession: session };
+      } else {
+        const orderId = await uuidv4();
+
+        await Promise.all(
+          products.map(async (product) => {
+            const item = await strapi
+              .service("api::product.product")
+              .findOne(product.id);
+
+            console.log("this is item------->", item);
+            console.log("this is product------->", product);
+
+            return {
+              price_data: {
+                currency: "bgn",
+                product_data: {
+                  name: item.name,
+                },
+                unit_amount: Math.round(item.price * 500),
+              },
+              quantity: product.quantity,
+            };
+          })
+        );
+
+        await strapi.service("api::order.order").create({
+          data: {
+            addressInfo,
+            products,
+            stripeId: undefined,
+            paymentMethod,
+            orderId,
+            status,
+            user,
+            totalPrice,
+          },
+        });
+
+        return {
+          products,
+          stripeId: undefined,
+          paymentMethod,
+          addressInfo,
+          user,
+          totalPrice,
+        };
+      }
+    } catch (error) {
+      ctx.response.status = 500;
+      return { error };
+    }
+  },
+}));
