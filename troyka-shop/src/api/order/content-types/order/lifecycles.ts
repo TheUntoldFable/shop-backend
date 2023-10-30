0a6e56a27f3cc8c53bb4686004988bd025096c60
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-10-30 17:02:27 +0200

    Send email to marti

--- troyka-shop/src/api/order/content-types/order/lifecycles.ts
+++ troyka-shop/src/api/order/content-types/order/lifecycles.ts
@@ -16,6 +16,24 @@ export default {
 			}
 		}
 
+		await strapi.plugins['email'].services.email.send({
+			to: 'pilyovmartin20@gmail.com',
+			from: 'info.troyka@gmail.com',
+			subject: 'Нова поръчка',
+			html: `<div><h2>Поръка №: ${result.orderId}</h2></br>
+				<div>Адрес:</div></br>
+				<div>Град: ${result.addressInfo.city}</div></br>
+				<div>Улица: ${result.addressInfo.street}</div></br>
+				<div>Номер на вход/сграда: ${result.addressInfo?.houseNumber}</div></br>
+				<div>Пощенски код: ${result.addressInfo?.postalCode}</div></br>
+				<div>Име: ${result.credentialsInfo?.firstName}</div></br>
+				<div>Фамилия: ${result.credentialsInfo?.secondName}</div></br>
+				<div>Мобилен: ${result.credentialsInfo?.phoneNumber}</div></br>
+				<div>Акаунт: ${result?.user}</div></br>
+				<div>Имейл: ${result.credentialsInfo?.email}</div></br>
+				<div>Офис адрес: ${result.addressInfo?.officeAddress}</div>
+				<h3>Стойност на поръчка: ${result.totalPrice} ${locale === 'bg' ? 'лв' : '€'}</h3></div>`})
+
 		if(result.paymentMethod !== 'card'){
 			try {
 				await strapi.plugins['email'].services.email.send({
